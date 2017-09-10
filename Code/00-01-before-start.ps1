4b07f8f589ad9d674adca35bbfb5919b9327e127
Ziemek Borowski <ziemek@ziembor.pl>
2017-09-10 10:45:58 +0200

    Delete 00-01-before-start.ps1

--- Code/00-01-before-start.ps1
+++
@@ -1,8 +0,0 @@
-Start-Transcript $env:Temp\GettingStarted.txt -Force
-
-Set-ExecutionPolicy RemoteSigned -Force -Scope CurrentUser
-
-get-host 
-
-Stop-Transcript
-notepad $env:Temp\GettingStarted.txt
